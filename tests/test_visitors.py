71cad4afa7005497e0347c22dfa6e1a33539ff8a
tristanlatr <tris.la.tr@gmail.com>
2021-08-26 22:05:17 -0400

    just testing

--- tests/test_visitors.py
+++ tests/test_visitors.py
@@ -1,11 +1,11 @@
 import pydocspec
-from pydocspec.visitors import PrintVisitor, FilterVisitor, walk_ApiObject
+from pydocspec.visitors import PrintVisitor, FilterVisitor
 from .fixtures import root1
 
 def test_visitors(capsys, root1: pydocspec.ApiObjectsRoot) -> None:
     module = root1.root_modules[0]
     visitor = PrintVisitor(colorize=False)
-    walk_ApiObject(module, visitor)
+    module.walk(visitor)
     captured = capsys.readouterr().out
     assert captured == """:0 - Module: a
 | :1 - Indirection: Union
@@ -17,13 +17,40 @@ def test_visitors(capsys, root1: pydocspec.ApiObjectsRoot) -> None:
 """
 
     predicate = lambda ob: not isinstance(ob, pydocspec.Data) # removes any Data entries
-
     filter_visitor = FilterVisitor(predicate)
-    walk_ApiObject(module, filter_visitor)
-    walk_ApiObject(module, visitor)
+    module.walk(filter_visitor)
+
+    module.walk(visitor)
     captured = capsys.readouterr().out
     assert captured == """:0 - Module: a
 | :1 - Indirection: Union
 | :2 - Class: foo
 | | :6 - Function: __init__
+"""
+
+def test_visitors2(capsys, root2: pydocspec.ApiObjectsRoot) -> None:
+    module = root2.root_modules[0]
+    visitor = PrintVisitor(colorize=False)
+    module.walk(visitor)
+    captured = capsys.readouterr().out
+    assert captured == """:0 - Module: a
+| :1 - Indirection: Union
+| :2 - Class: foo
+| | :4 - Data: _val
+| | :5 - Data: _alias
+| | :6 - Function: __init__
+| :8 - Data: saila
+"""
+    # removes entries starting by one underscore that are not dunder methods, aka private API.
+    predicate = lambda ob: not ob.name.startswith("_") or ob.name.startswith("__") and ob.name.endswith("__")
+    filter_visitor = FilterVisitor(predicate)
+    module.walk(filter_visitor)
+
+    module.walk(visitor)
+    captured = capsys.readouterr().out
+    assert captured == """:0 - Module: a
+| :1 - Indirection: Union
+| :2 - Class: foo
+| | :6 - Function: __init__
+| :8 - Data: saila
 """
