a3101bba91c94c9f478e3036ffc4c5d0a6022cab
tristanlatr <tris.la.tr@gmail.com>
2021-09-03 13:26:09 -0400

    Add cyclic imports test.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,8 +3,16 @@ from typing import TYPE_CHECKING
 import sys
 import pytest
 
+from pydocspec import specfactory
+
 posonlyargs = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8")
 typecomment = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8")
+
+default_factory = specfactory.Factory.default()
+no_brain_factory = specfactory.Factory.default(load_brains=False)
+rootcls_param = pytest.mark.parametrize(
+    'rootcls', (no_brain_factory.ApiObjectsRoot, default_factory.ApiObjectsRoot)
+    )
 
 # Because pytest 6.1 does not yet export types for fixtures, we define
 # approximations that are good enough for our test cases:
