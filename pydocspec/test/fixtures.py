31b7224d460c54b0088b3fc4c35b72bb14d3c24a
tristanlatr <tris.la.tr@gmail.com>
2022-04-28 14:23:49 -0400

    fix tox -e test-no-docspec

--- pydocspec/test/fixtures.py
+++ pydocspec/test/fixtures.py
@@ -1,111 +1,109 @@
 import pytest
-from typing import TypeVar, Union
 
 import pydocspec
-from pydocspec import converter, _docspec
+from pydocspec import _docspec
 
 if _docspec.upstream.docspec is not None:
   import docspec
-else:
-  docspec = _docspec
+  from pydocspec import converter
 
-_mod1 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
-    docspec.Indirection(name='Union', location=docspec.Location('test.py', 1), docstring=None, target='typing.Union'),
-    docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
-      docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
-      metaclass=None, bases=None, decorations=None, members=[
-      docspec.Variable(name='val', location=docspec.Location('test.py', 4), 
-        docstring=None, datatype='Union[int, float]', value='42'),
-      docspec.Variable(name='alias', location=docspec.Location('test.py', 5), docstring=None, datatype=None, value='val'),
-      docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
-        docspec.Argument(name='self', location=docspec.Location('test.py', 6), 
-          type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
+  _mod1 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
+      docspec.Indirection(name='Union', location=docspec.Location('test.py', 1), docstring=None, target='typing.Union'),
+      docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
+        docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
+        metaclass=None, bases=None, decorations=None, members=[
+        docspec.Variable(name='val', location=docspec.Location('test.py', 4), 
+          docstring=None, datatype='Union[int, float]', value='42'),
+        docspec.Variable(name='alias', location=docspec.Location('test.py', 5), docstring=None, datatype=None, value='val'),
+        docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
+          docspec.Argument(name='self', location=docspec.Location('test.py', 6), 
+            type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
+        ], return_type=None, decorations=None),
+      ]),
+      docspec.Variable(name='saila', location=docspec.Location('test.py', 8), docstring=None, datatype=None, value='foo.alias'),
+    ])
+  _mod1.sync_hierarchy()
+
+  _mod2 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
+      docspec.Indirection(name='Union', location=docspec.Location('test.py', 1), docstring=None, target='typing.Union'),
+      docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
+        docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
+        metaclass=None, bases=None, decorations=None, members=[
+        docspec.Variable(name='val', location=docspec.Location('test.py', 4), 
+          docstring=None, datatype='Union[int, float]', value='42'),
+        docspec.Variable(name='alias', location=docspec.Location('test.py', 5), docstring=None, datatype=None, value='val'),
+        docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
+          docspec.Argument(name='self', location=docspec.Location('test.py', 6), 
+            type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
+        ], return_type=None, decorations=None),
+      ]),
+      docspec.Class(name='foosub', location=docspec.Location('test.py', 8), 
+        docstring=docspec.Docstring(content='This is subclass of class foo.', location=docspec.Location('test.py', 9)), 
+        metaclass=None, bases=['foo'], decorations=None, members=[
+        docspec.Function(name='__init__', location=docspec.Location('test.py', 10), docstring=None, modifiers=None, args=[
+          docspec.Argument(name='self', location=docspec.Location('test.py', 10), 
+            type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
+        ], return_type=None, decorations=None),
+      ]),
+      docspec.Variable(name='saila', location=docspec.Location('test.py', 12), docstring=None, datatype=None, value='foo.alias'),
+    ])
+  _mod2.sync_hierarchy()
+
+  _mod3 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
+      docspec.Indirection(name='Union', location=docspec.Location('test.py', 1), docstring=None, target='typing.Union'),
+      docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
+        docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
+        metaclass=None, bases=None, decorations=None, members=[
+        docspec.Variable(name='_val', location=docspec.Location('test.py', 4), docstring=None, datatype='Union[int, float]', value='42'),
+        docspec.Variable(name='_alias', location=docspec.Location('test.py', 5), docstring=None, datatype=None, value='_val'),
+        docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
+          docspec.Argument(name='self', location=docspec.Location('test.py', 6), 
+            type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
       ], return_type=None, decorations=None),
-    ]),
-    docspec.Variable(name='saila', location=docspec.Location('test.py', 8), docstring=None, datatype=None, value='foo.alias'),
-  ])
-_mod1.sync_hierarchy()
+      ]),
+      docspec.Variable(name='saila', location=docspec.Location('test.py', 8), docstring=None, datatype=None, value='foo.alias'),
+    ])
 
-_mod2 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
-    docspec.Indirection(name='Union', location=docspec.Location('test.py', 1), docstring=None, target='typing.Union'),
-    docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
-      docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
-      metaclass=None, bases=None, decorations=None, members=[
-      docspec.Variable(name='val', location=docspec.Location('test.py', 4), 
-        docstring=None, datatype='Union[int, float]', value='42'),
-      docspec.Variable(name='alias', location=docspec.Location('test.py', 5), docstring=None, datatype=None, value='val'),
-      docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
-        docspec.Argument(name='self', location=docspec.Location('test.py', 6), 
-          type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
-      ], return_type=None, decorations=None),
-    ]),
-    docspec.Class(name='foosub', location=docspec.Location('test.py', 8), 
-      docstring=docspec.Docstring(content='This is subclass of class foo.', location=docspec.Location('test.py', 9)), 
-      metaclass=None, bases=['foo'], decorations=None, members=[
-      docspec.Function(name='__init__', location=docspec.Location('test.py', 10), docstring=None, modifiers=None, args=[
-        docspec.Argument(name='self', location=docspec.Location('test.py', 10), 
-          type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
-      ], return_type=None, decorations=None),
-    ]),
-    docspec.Variable(name='saila', location=docspec.Location('test.py', 12), docstring=None, datatype=None, value='foo.alias'),
-  ])
-_mod2.sync_hierarchy()
+  _mod3.sync_hierarchy()
 
-_mod3 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
-    docspec.Indirection(name='Union', location=docspec.Location('test.py', 1), docstring=None, target='typing.Union'),
-    docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
-      docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
-      metaclass=None, bases=None, decorations=None, members=[
-      docspec.Variable(name='_val', location=docspec.Location('test.py', 4), docstring=None, datatype='Union[int, float]', value='42'),
-      docspec.Variable(name='_alias', location=docspec.Location('test.py', 5), docstring=None, datatype=None, value='_val'),
-      docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
-        docspec.Argument(name='self', location=docspec.Location('test.py', 6), 
-          type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None)
-    ], return_type=None, decorations=None),
-    ]),
-    docspec.Variable(name='saila', location=docspec.Location('test.py', 8), docstring=None, datatype=None, value='foo.alias'),
-  ])
+  _mod4 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
+      docspec.Function(
+        name='f',
+        location=docspec.Location('test.py', 2),
+        docstring=docspec.Docstring(content='This uses annotations and keyword-only arguments.', location=docspec.Location('test.py', 2)),
+        modifiers=None,
+        args=[
+          docspec.Argument(name='a', location=docspec.Location('test.py', 2), type=docspec.Argument.Type.Positional, decorations=None, datatype='int', default_value=None),
+          docspec.Argument(name='c', location=docspec.Location('test.py', 2), type=docspec.Argument.Type.KeywordOnly, decorations=None, datatype='str', default_value=None),
+          docspec.Argument(name='opts', location=docspec.Location('test.py', 2), type=docspec.Argument.Type.KeywordRemainder, decorations=None, datatype='Any', default_value=None),
+        ],
+        return_type='None',
+        decorations=[],
+      ),
+      docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
+        docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
+        metaclass=None, bases=None, decorations=None, members=[
+        docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
+          docspec.Argument(name='self', location=docspec.Location('test.py', 6), type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None),
+          docspec.Argument(name='port', location=docspec.Location('test.py', 6), type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value='8001'),
+        ], return_type=None, decorations=None),
+      ]),
+    ])
+  _mod4.sync_hierarchy()
 
-_mod3.sync_hierarchy()
+  @pytest.fixture
+  def mod1() -> docspec.Module:
+    return _mod1
 
-_mod4 = docspec.Module(name='a', location=docspec.Location('test.py', 0), docstring=None, members=[
-    docspec.Function(
-      name='f',
-      location=docspec.Location('test.py', 2),
-      docstring=docspec.Docstring(content='This uses annotations and keyword-only arguments.', location=docspec.Location('test.py', 2)),
-      modifiers=None,
-      args=[
-        docspec.Argument(name='a', location=docspec.Location('test.py', 2), type=docspec.Argument.Type.Positional, decorations=None, datatype='int', default_value=None),
-        docspec.Argument(name='c', location=docspec.Location('test.py', 2), type=docspec.Argument.Type.KeywordOnly, decorations=None, datatype='str', default_value=None),
-        docspec.Argument(name='opts', location=docspec.Location('test.py', 2), type=docspec.Argument.Type.KeywordRemainder, decorations=None, datatype='Any', default_value=None),
-      ],
-      return_type='None',
-      decorations=[],
-    ),
-    docspec.Class(name='foo', location=docspec.Location('test.py', 2), 
-      docstring=docspec.Docstring(content='This is class foo.', location=docspec.Location('test.py', 3)), 
-      metaclass=None, bases=None, decorations=None, members=[
-      docspec.Function(name='__init__', location=docspec.Location('test.py', 6), docstring=None, modifiers=None, args=[
-        docspec.Argument(name='self', location=docspec.Location('test.py', 6), type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value=None),
-        docspec.Argument(name='port', location=docspec.Location('test.py', 6), type=docspec.Argument.Type.Positional, decorations=None, datatype=None, default_value='8001'),
-      ], return_type=None, decorations=None),
-    ]),
-  ])
-_mod4.sync_hierarchy()
-
-@pytest.fixture
-def mod1() -> docspec.Module:
-  return _mod1
-
-@pytest.fixture
-def root1() -> pydocspec.TreeRoot:
-  return converter.convert_docspec_modules([_mod1])
-@pytest.fixture
-def root2() -> pydocspec.TreeRoot:
-  return converter.convert_docspec_modules([_mod2])
-@pytest.fixture
-def root3() -> pydocspec.TreeRoot:
-  return converter.convert_docspec_modules([_mod3])
-@pytest.fixture
-def root4() -> pydocspec.TreeRoot:
-  return converter.convert_docspec_modules([_mod4])
+  @pytest.fixture
+  def root1() -> pydocspec.TreeRoot:
+    return converter.convert_docspec_modules([_mod1])
+  @pytest.fixture
+  def root2() -> pydocspec.TreeRoot:
+    return converter.convert_docspec_modules([_mod2])
+  @pytest.fixture
+  def root3() -> pydocspec.TreeRoot:
+    return converter.convert_docspec_modules([_mod3])
+  @pytest.fixture
+  def root4() -> pydocspec.TreeRoot:
+    return converter.convert_docspec_modules([_mod4])
--- pydocspec/test/test_pydocspec.py
+++ pydocspec/test/test_pydocspec.py
@@ -2,127 +2,126 @@ import re
 from typing import Optional
 
 import pytest
-from pydocspec import converter, astroidutils, processor, _docspec
+from pydocspec import astroidutils, processor, _docspec
 import pydocspec
 
 if _docspec.upstream.docspec is not None:
+    from pydocspec import converter
     import docspec
-else:
-    docspec = _docspec
 
-from .fixtures import mod1, root2, root4
-from . import ModFromTextFunction, mod_from_text_param, _default_astbuilder
+    from .fixtures import mod1, root2, root4
+    from . import ModFromTextFunction, mod_from_text_param, _default_astbuilder
 
-def test_expand_name(mod1: docspec.Module) -> None:
-    root = converter.convert_docspec_modules([mod1])
+    def test_expand_name(mod1: docspec.Module) -> None:
+        root = converter.convert_docspec_modules([mod1])
 
-    saila = root.all_objects['a.saila']
-    alias = root.all_objects['a.foo.alias']
+        saila = root.all_objects['a.saila']
+        alias = root.all_objects['a.foo.alias']
 
-    assert isinstance(saila, pydocspec.Variable)
-    assert isinstance(alias, pydocspec.Variable)
+        assert isinstance(saila, pydocspec.Variable)
+        assert isinstance(alias, pydocspec.Variable)
 
-    assert processor.data_attr.is_alias(saila)
-    assert processor.data_attr.is_alias(alias)
-    assert saila.is_alias
-    assert alias.is_alias
+        assert processor.data_attr.is_alias(saila)
+        assert processor.data_attr.is_alias(alias)
+        assert saila.is_alias
+        assert alias.is_alias
 
-    mod = root.all_objects['a']
+        mod = root.all_objects['a']
 
-    assert mod.expand_name('Union') == 'typing.Union'
-    assert mod.expand_name('foo.alias') == 'a.foo.val'
-    assert mod.expand_name('saila') == 'a.foo.val'
+        assert mod.expand_name('Union') == 'typing.Union'
+        assert mod.expand_name('foo.alias') == 'a.foo.val'
+        assert mod.expand_name('saila') == 'a.foo.val'
 
-    klass = root.all_objects['a.foo']
+        klass = root.all_objects['a.foo']
 
-    assert klass.expand_name('alias') == 'a.foo.val'
-    assert klass.expand_name('saila') == 'a.foo.val'
-    assert klass.expand_name('Union') == 'typing.Union'
+        assert klass.expand_name('alias') == 'a.foo.val'
+        assert klass.expand_name('saila') == 'a.foo.val'
+        assert klass.expand_name('Union') == 'typing.Union'
 
-def test_expand_name_subclass(root2: pydocspec.TreeRoot) -> None:
-    root = root2
+    def test_expand_name_subclass(root2: pydocspec.TreeRoot) -> None:
+        root = root2
 
-    subklass = root.all_objects['a.foosub']
-    assert isinstance(subklass, pydocspec.Class)
+        subklass = root.all_objects['a.foosub']
+        assert isinstance(subklass, pydocspec.Class)
 
-    subklass.find('alias') == root.all_objects['a.foo.alias']
+        subklass.find('alias') == root.all_objects['a.foo.alias']
 
-    klass = root.all_objects['a.foo']
-    assert isinstance(klass, pydocspec.Class)
-    assert klass.subclasses[0] == subklass
+        klass = root.all_objects['a.foo']
+        assert isinstance(klass, pydocspec.Class)
+        assert klass.subclasses[0] == subklass
 
-    assert subklass.expand_name('foosub.alias') == 'a.foo.val'
-    assert subklass.expand_name('foo.alias') == 'a.foo.val'
-    assert subklass.expand_name('saila') == 'a.foo.val'
-    assert subklass.expand_name('Union') == 'typing.Union'
+        assert subklass.expand_name('foosub.alias') == 'a.foo.val'
+        assert subklass.expand_name('foo.alias') == 'a.foo.val'
+        assert subklass.expand_name('saila') == 'a.foo.val'
+        assert subklass.expand_name('Union') == 'typing.Union'
 
-def test_signature(root4: pydocspec.TreeRoot) -> None:
-    root = root4
+    def test_signature(root4: pydocspec.TreeRoot) -> None:
+        root = root4
 
-    func = root.all_objects['a.f']
-    assert isinstance(func, pydocspec.Function)
-    assert str(func.signature()) == "(a: int, *, c: str, **opts: Any) -> None"
-    assert str(func.signature(include_types=False)) == "(a, *, c, **opts) -> None"
-    assert str(func.signature(include_types=False, include_return_type=False)) == "(a, *, c, **opts)"
+        func = root.all_objects['a.f']
+        assert isinstance(func, pydocspec.Function)
+        assert str(func.signature()) == "(a: int, *, c: str, **opts: Any) -> None"
+        assert str(func.signature(include_types=False)) == "(a, *, c, **opts) -> None"
+        assert str(func.signature(include_types=False, include_return_type=False)) == "(a, *, c, **opts)"
 
-    init_method = root.all_objects['a.foo.__init__']
-    assert isinstance(init_method, pydocspec.Function)
-    assert init_method.is_method==True
-    assert str(init_method.signature()) == "(self, port=8001)"
-    assert str(init_method.signature(include_self=False)) == "(port=8001)"
-    assert str(init_method.signature(include_self=False, include_defaults=False)) == "(port)"
+        init_method = root.all_objects['a.foo.__init__']
+        assert isinstance(init_method, pydocspec.Function)
+        assert init_method.is_method==True
+        assert str(init_method.signature()) == "(self, port=8001)"
+        assert str(init_method.signature(include_self=False)) == "(port=8001)"
+        assert str(init_method.signature(include_self=False, include_defaults=False)) == "(port)"
 
-@mod_from_text_param
-def test_node2fullname(mod_from_text:ModFromTextFunction) -> None:
-    """The node2fullname() function finds the full (global) name for
-    a name expression in the AST.
-    """
-    # https://github.com/NiklasRosenstein/docspec/issues/34
-    # mod = mod_from_text('''
-    # class session:
-    #     from twisted.conch.interfaces import ISession
-    #     ''', modname='test')
+    @mod_from_text_param
+    def test_node2fullname(mod_from_text:ModFromTextFunction) -> None:
+        """The node2fullname() function finds the full (global) name for
+        a name expression in the AST.
+        """
+        # https://github.com/NiklasRosenstein/docspec/issues/34
+        # mod = mod_from_text('''
+        # class session:
+        #     from twisted.conch.interfaces import ISession
+        #     ''', modname='test')
 
-    mod = mod_from_text('''
-    from twisted.conch.interfaces import ISession
-    class session:
-        ISession=ISession
-        ''', modname='test')
+        mod = mod_from_text('''
+        from twisted.conch.interfaces import ISession
+        class session:
+            ISession=ISession
+            ''', modname='test')
 
-    def lookup(expr: str) -> Optional[str]:
-        return astroidutils.node2fullname(astroidutils.extract_expr(expr), mod)
+        def lookup(expr: str) -> Optional[str]:
+            return astroidutils.node2fullname(astroidutils.extract_expr(expr), mod)
 
-    # None is returned for non-name nodes.
-    assert lookup('123') is None
-    # Local names are returned with their full name.
-    assert lookup('session') == 'test.session'
-    # A name that has no match at the top level is returned as-is.
-    assert lookup('nosuchname') == 'nosuchname'
-    # Unknown names are resolved as far as possible.
-    assert lookup('session.nosuchname') == 'test.session.nosuchname'
-    # Aliases are resolved on local names.
-    assert lookup('session.ISession') == 'twisted.conch.interfaces.ISession'
-    # Aliases are resolved on global names.
-    assert lookup('test.session.ISession') == 'twisted.conch.interfaces.ISession'
+        # None is returned for non-name nodes.
+        assert lookup('123') is None
+        # Local names are returned with their full name.
+        assert lookup('session') == 'test.session'
+        # A name that has no match at the top level is returned as-is.
+        assert lookup('nosuchname') == 'nosuchname'
+        # Unknown names are resolved as far as possible.
+        assert lookup('session.nosuchname') == 'test.session.nosuchname'
+        # Aliases are resolved on local names.
+        assert lookup('session.ISession') == 'twisted.conch.interfaces.ISession'
+        # Aliases are resolved on global names.
+        assert lookup('test.session.ISession') == 'twisted.conch.interfaces.ISession'
 
-def test_arguments_required_at_init() -> None:
-    mod = _default_astbuilder.mod_from_text('')
+    def test_arguments_required_at_init() -> None:
+        mod = _default_astbuilder.mod_from_text('')
 
-    with pytest.raises(TypeError):
-        mod.root.factory.Class(name='mycls', # type:ignore
-                                location=None, 
-                                docstring=None, 
-                                metaclass=None,
-                                bases=None,
-                                decorations=None,
-                                members=[])
-    
-    with pytest.raises(TypeError):
-        mod.root.factory.Function( # type:ignore
-                                name='myfunc', 
-                                location=None, 
-                                docstring=None, 
-                                args=[], 
-                                modifiers=[], 
-                                return_type='str', 
-                                decorations=[],)
+        with pytest.raises(TypeError):
+            mod.root.factory.Class(name='mycls', # type:ignore
+                                    location=None, 
+                                    docstring=None, 
+                                    metaclass=None,
+                                    bases=None,
+                                    decorations=None,
+                                    members=[])
+        
+        with pytest.raises(TypeError):
+            mod.root.factory.Function( # type:ignore
+                                    name='myfunc', 
+                                    location=None, 
+                                    docstring=None, 
+                                    args=[], 
+                                    modifiers=[], 
+                                    return_type='str', 
+                                    decorations=[],)
